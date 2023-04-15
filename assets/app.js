d479df644c3539ef27ac203d41c4951732b1e8a7
mattbuc <mattbuc98@gmail.com>
2023-04-15 18:03:11 +0200

    Merge

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,20 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+<<<<<<< HEAD
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+=======
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+import './styles/__colors.scss';
+import './styles/__mixins.scss';
+import './styles/__icon_btn.scss';
+import './styles/__icon_cta.scss';
+import './styles/__link.scss.scss';
+// start the Stimulus application
+import './bootstrap';
+>>>>>>> main
--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
