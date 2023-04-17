0f4750cf98af54ca3d6ccf5e1edf858eb79e1b17
Evan <evan.bombart@etudiant.univ-reims.fr>
2023-04-17 23:46:30 +0200

    ss

--- src/Controller/StatsController.php
+++ src/Controller/StatsController.php
@@ -16,9 +16,16 @@ class StatsController extends AbstractController
     )]
     public function index(StatsRepository $statsRepository, VshistoryRepository $vshistoryRepository): Response
     {
+//        Si les stats sont vides renvoyer pas de stats sinon renvoyer les stats
+        $stats = $statsRepository->findOneBy(['id' => '1']);
+        if ($stats === null) {
+            $returnStats = 'No stats';
+        }else{
+            $returnStats = $stats;
+        }
         return $this->render('stats/index.html.twig', [
             'controller_name' => 'StatsController',
-            'stats' => $statsRepository->findOneBy(['id' => '1']),
+            'stats' => $returnStats,
             'vs' => $vshistoryRepository->findAll(),
         ]);
     }
